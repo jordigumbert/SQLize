43b7c66aa78c02d97d06839c1b3846bf1c1dd917
Jorgen <debianjorgen@gmail.com>
2026-04-22 18:59:52 +0200

    fix: conexio amb github

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ import 'dotenv/config'   // mode ESM
 
 import { TestConnection } from "./funcions/database.js";
 
-
-console.log(process.env.DB_PASS, process.env.DB_USER)
-
-const sequelize = new Sequelize('SEQUELIZE', process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize( process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS, {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
 });
-
 
 try { 
     await TestConnection(sequelize);
 } catch (err) {
     console.log("[ERR]:: ", err);
 };
+
+process.env.DB_SCHEMA
+'SEQUELIZE'
